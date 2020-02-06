91f197ccf516becf7832f601d3c03840732ea105
Isaac Schemm <isaacschemm@gmail.com>
2020-02-06 16:02:31 +0000

    Add BearerToken

---
+++ InoreaderFs/Authentication/BearerToken.fs
@@ -0,0 +1,9 @@
+﻿namespace InoreaderFs.Authentication
+
+type BearerToken = {
+    token: string
+} with
+    interface IInoreaderCredentials with
+        member this.GetHeaders() = seq {
+            ("Authorization", sprintf "Bearer %s" this.token)
+        }
