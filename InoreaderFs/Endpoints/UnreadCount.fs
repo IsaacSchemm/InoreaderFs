3bc7bbafb3352dfbf23dfaa453bcffd6240514bc
Isaac Schemm <isaacschemm@gmail.com>
2020-02-06 20:08:49 +0000

    Add UnreadCount

---
+++ InoreaderFs/Endpoints/UnreadCount.fs
@@ -0,0 +1,41 @@
+﻿namespace InoreaderFs.Endpoints
+
+open System
+open System.IO
+open FSharp.Json
+open InoreaderFs
+
+module UnreadCount =
+    type Count = {
+        id: string
+        count: obj
+        newestItemTimestampUsec: string
+    } with
+        member this.CountAsInt32 =
+            this.count
+            |> sprintf "%O"
+            |> Int32.Parse
+        member this.NewestItemTimestamp =
+            match Int64.TryParse this.newestItemTimestampUsec with
+            | (true, x) -> DateTimeOffset.FromUnixTimeMilliseconds(0L) + new TimeSpan(x * 10L)
+            | _ -> DateTimeOffset.MinValue
+
+    type Response = {
+        max: string
+        unreadcounts: Count list
+    }
+
+    let AsyncExecute credentials = async {
+        let req = new InoreaderRequest("/reader/api/0/unread-count")
+
+        use! resp = req.AsyncGetResponse credentials
+        use respStream = resp.GetResponseStream()
+        use sr = new StreamReader(respStream)
+
+        let! json = sr.ReadToEndAsync() |> Async.AwaitTask
+        return Json.deserializeEx<Response> (JsonConfig.create(allowUntyped = true)) json
+    }
+
+    let ExecuteAsync credentials =
+        AsyncExecute credentials
+        |> Async.StartAsTask
