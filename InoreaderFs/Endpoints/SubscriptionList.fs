d8517465469a269071e56b96bd7b7629485220a4
Isaac Schemm <isaacschemm@gmail.com>
2020-02-07 18:29:55 +0000

    Add SubscriptionList

---
+++ InoreaderFs/Endpoints/SubscriptionList.fs
@@ -0,0 +1,42 @@
+﻿namespace InoreaderFs.Endpoints
+
+open System
+open System.IO
+open FSharp.Json
+open InoreaderFs
+
+module SubscriptionList =
+    type Category = {
+        id: string
+        label: string
+    }
+
+    type Subscription = {
+        id: string
+        title: string
+        categories: Category list
+        sortid: string
+        firstitemmsec: int64
+        url: string
+        htmlUrl: string
+        iconUrl: string
+    }
+
+    type Response = {
+        subscriptions: Subscription list
+    }
+
+    let AsyncExecute credentials = async {
+        let req = new InoreaderRequest("/reader/api/0/subscription/list")
+
+        use! resp = req.AsyncGetResponse credentials
+        use respStream = resp.GetResponseStream()
+        use sr = new StreamReader(respStream)
+
+        let! json = sr.ReadToEndAsync() |> Async.AwaitTask
+        return Json.deserializeEx<Response> (JsonConfig.create(allowUntyped = true)) json
+    }
+
+    let ExecuteAsync credentials =
+        AsyncExecute credentials
+        |> Async.StartAsTask
